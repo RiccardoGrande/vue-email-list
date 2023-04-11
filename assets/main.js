ee7fe0ec9f44bc127c26e6f8c72f23113bc6d3e0
RiccardoGrande <grandericcardo1997@gmail.com>
2023-04-11 17:11:43 +0200

    add

--- assets/main.js
+++ assets/main.js
@@ -12,15 +12,19 @@ createApp({
     
     //console.log(axios.get(this.url)); // Promise
     // this.url (uri+request body)
-    axios
-    .get(this.url)
-    .then(response => {
-      // prendo l'intero responso (solo i dati) e lo salvo nell'oggetto vue
-      console.log(response); // contiene un oggetto 
-      //console.log(response.data);
-      this.mails = response.data 
-     
-    })
+    for (let index = 0; index < 10; index++) {
+      axios
+      .get(this.url)
+      .then(response => {
+        // prendo l'intero responso (solo i dati) e lo salvo nell'oggetto vue
+        console.log(response); // contiene un oggetto 
+        //console.log(response.data);
+        this.mails = response.data
+       
+      })
+      
+    }
+   
 
   }
 }).mount('#app')
